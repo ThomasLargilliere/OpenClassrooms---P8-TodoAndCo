a1e8893cfaf48ce6dd22254bdf704cb0ff92cc29
Thomas Largilliere <thomas.largilliere2012@gmail.com>
2022-09-09 16:26:58 +0200

    Test Task getId

---
+++ tests/Entity/TaskTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Task;
+use App\Repository\TaskRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class TaskTest extends KernelTestCase
+{
+    private TaskRepository $taskRepository;
+
+    protected function setUp(): void
+    {
+        $this->taskRepository = static::getContainer()->get(TaskRepository::class);
+    }
+
+    public function testGetId()
+    {
+        // GIVEN
+        $task = $this->taskRepository->findOneById(1);
+
+        // WHEN
+        $result = $task->getId();
+
+        // THEN
+        $this->assertIsInt($result);
+    }
+}
