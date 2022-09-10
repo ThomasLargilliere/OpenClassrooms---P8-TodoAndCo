873ca31195c956475361279fb502a78ca39a7606
Thomas Largilliere <thomas.largilliere2012@gmail.com>
2022-09-10 09:37:51 +0200

    Test TaskController createTask

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -12,4 +12,27 @@ class TaskControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/tasks');
         $this->assertResponseIsSuccessful();
     }
+
+    public function testCreateTaskForGetForm()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/tasks/create');
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testCreateTaskForSubmitFormWhenUserIsNotConnected()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/tasks/create');
+
+        $form = $crawler->selectButton('Ajouter')->form([
+            'task[title]' => 'Task Title',
+            'task[content]' => 'Task Content'
+        ]);
+
+        $client->submit($form);
+        $this->assertResponseRedirects('/login');
+        $client->followRedirect();
+        $this->assertSelectorExists('.alert.alert-danger');
+    }
 }
