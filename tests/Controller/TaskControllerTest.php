55aa6f87e6177890c444824bbdfdae44ebbf699f
Thomas Largilliere <thomas.largilliere2012@gmail.com>
2022-09-10 10:00:10 +0200

    Test TaskController ToggleTask

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -3,33 +3,42 @@
 namespace App\Tests\Service;
 
 use App\Repository\UserRepository;
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TaskControllerTest extends WebTestCase
 {
     private UserRepository $userRepository;
+    private TaskRepository $taskRepository;
 
     protected function setUp(): void
     {
         $this->client = static::createClient();
         $this->userRepository = static::getContainer()->get(UserRepository::class);
+        $this->taskRepository = static::getContainer()->get(TaskRepository::class);
     }
-
 
     public function testListTask()
     {
+        // WHEN
         $crawler = $this->client->request('GET', '/tasks');
+
+        // THEN
         $this->assertResponseIsSuccessful();
     }
 
     public function testCreateTaskForGetForm()
     {
+        // WHEN
         $crawler = $this->client->request('GET', '/tasks/create');
+
+        // THEN
         $this->assertResponseIsSuccessful();
     }
 
     public function testCreateTaskForSubmitFormWhenUserIsNotConnected()
     {
+        // GIVEN
         $crawler = $this->client->request('GET', '/tasks/create');
 
         $form = $crawler->selectButton('Ajouter')->form([
@@ -37,7 +46,10 @@ class TaskControllerTest extends WebTestCase
             'task[content]' => 'Task Content'
         ]);
 
+        // WHEN
         $this->client->submit($form);
+
+        //THEN
         $this->assertResponseRedirects('/login');
         $this->client->followRedirect();
         $this->assertSelectorExists('.alert.alert-danger');
@@ -45,6 +57,7 @@ class TaskControllerTest extends WebTestCase
 
     public function testCreateTaskForSubmitFormWhenUserIsConnected()
     {
+        // GIVEN
         $testUser = $this->userRepository->findOneByEmail('admin@admin.fr');
         $this->client->loginUser($testUser);
 
@@ -55,7 +68,33 @@ class TaskControllerTest extends WebTestCase
             'task[content]' => 'Task Content'
         ]);
 
+        // WHEN
+
         $this->client->submit($form);
+
+        // THEN
+        $this->assertResponseRedirects('/tasks');
+        $this->client->followRedirect();
+        $this->assertSelectorExists('.alert.alert-success');
+    }
+
+    public function testToggleTaskWhenTaskIsDone()
+    {
+        // WHEN
+        $crawler = $this->client->request('GET', '/tasks/1/toggle');
+
+        // THEN
+        $this->assertResponseRedirects('/tasks');
+        $this->client->followRedirect();
+        $this->assertSelectorExists('.alert.alert-danger');
+    }
+
+    public function testToggleTaskWhenTaskIsNotDone()
+    {
+        // WHEN
+        $crawler = $this->client->request('GET', '/tasks/3/toggle');
+
+        // THEN
         $this->assertResponseRedirects('/tasks');
         $this->client->followRedirect();
         $this->assertSelectorExists('.alert.alert-success');
