ff5db7f1de6a420ace5c980e0402d6b0c280339a
Thomas Largilliere <thomas.largilliere2012@gmail.com>
2022-09-10 09:51:55 +0200

    Test TaskController createTask

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -2,37 +2,62 @@
 
 namespace App\Tests\Service;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TaskControllerTest extends WebTestCase
 {
+    private UserRepository $userRepository;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->userRepository = static::getContainer()->get(UserRepository::class);
+    }
+
+
     public function testListTask()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/tasks');
+        $crawler = $this->client->request('GET', '/tasks');
         $this->assertResponseIsSuccessful();
     }
 
     public function testCreateTaskForGetForm()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/tasks/create');
+        $crawler = $this->client->request('GET', '/tasks/create');
         $this->assertResponseIsSuccessful();
     }
 
     public function testCreateTaskForSubmitFormWhenUserIsNotConnected()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/tasks/create');
+        $crawler = $this->client->request('GET', '/tasks/create');
 
         $form = $crawler->selectButton('Ajouter')->form([
             'task[title]' => 'Task Title',
             'task[content]' => 'Task Content'
         ]);
 
-        $client->submit($form);
+        $this->client->submit($form);
         $this->assertResponseRedirects('/login');
-        $client->followRedirect();
+        $this->client->followRedirect();
         $this->assertSelectorExists('.alert.alert-danger');
+    }
+
+    public function testCreateTaskForSubmitFormWhenUserIsConnected()
+    {
+        $testUser = $this->userRepository->findOneByEmail('admin@admin.fr');
+        $this->client->loginUser($testUser);
+
+        $crawler = $this->client->request('GET', '/tasks/create');
+
+        $form = $crawler->selectButton('Ajouter')->form([
+            'task[title]' => 'Task Title',
+            'task[content]' => 'Task Content'
+        ]);
+
+        $this->client->submit($form);
+        $this->assertResponseRedirects('/tasks');
+        $this->client->followRedirect();
+        $this->assertSelectorExists('.alert.alert-success');
     }
 }
